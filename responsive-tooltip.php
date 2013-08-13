24fd1d9aa00417cea895aaf721fd6f1e156dc277
ItayXD <itayblabla@gmail.com>
2013-08-13 21:25:56 +0300

    Blinking tooltip bug fixed. Vesrion 1.2

--- responsive-tooltip.php
+++ responsive-tooltip.php
@@ -3,13 +3,12 @@
    Plugin Name: Responsive Mobile-Friendly Tooltip
    Plugin URI: https://github.com/ItayXD/responsive-tooltip
    Description: Responsive and mobile-friendly tooltip to present tiny amount of hidden content - the tip.
-   Version: 1.1
-   Author: ItayXD; Osvaldas;
+   Version: 1.2
+   Author: ItayXD;
    Author URI: itayxd.com
    License: GPLv2 or later
 	License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
-
 /*-------------------- Adds the shortcode --------------------*/
 
 function RMFtooltip_shortcode_function($atts, $content = null) {
@@ -64,5 +63,21 @@ function RMFtooltip_stylesheet_js() {
 	wp_register_script( 'RMFtooltip-js', plugins_url('responsive-tooltip.js', __FILE__), 'jquery', null, true );
 	wp_enqueue_script( 'RMFtooltip-js' );
 }
-
-
+/*-------------------- Version Handling --------------------*/
+$c_version = 1.2; // Current version
+$o_version = get_option(RMFtooltip_version); //Gets old version
+if ( $c_version > $o_version ) {
+   update_option('RMFtooltip_version', $c_version);
+   $RMFtooltip_style_settings = get_option('RMFtooltip_style_settings'); //Genrate user's custom CSS file
+   if ($RMFtooltip_style_settings[chkbx_use_custom_css] == 'on') {
+      if ($RMFtooltip_style_settings[chkbx_replace_css] == 'on') {
+         $css_file = $RMFtooltip_style_settings[textarea_css]; //Writes only the new changes
+       } else {
+         $css_file = file_get_contents(plugin_dir_path(__FILE__) . 'responsive-tooltip.org.css');
+          $css_file .= "\n{$RMFtooltip_style_settings[textarea_css]}"; //Adds the entered code at the end of the original code
+       }
+   } else { //Else writes the original file
+      $css_file = file_get_contents(plugin_dir_path(__FILE__) . 'responsive-tooltip.org.css'); //Writes only the original file
+   }
+   file_put_contents(plugin_dir_path(__FILE__) . 'responsive-tooltip.css', $css_file, LOCK_EX);
+}
